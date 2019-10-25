65d26fde76813e7f589b328fb8dd855abcc09a73
Kames <info@kamescg.com>
2019-10-25 08:38:08 -0400

    update config files

--- packages/apps/static/gatsby-config.js
+++ packages/apps/static/gatsby-config.js
@@ -38,6 +38,30 @@ module.exports = {
         },
       },
     },
+
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        "fonts": [
+          {
+            "family": "Roboto",
+            "variants": [
+              "400",
+              "400i",
+              "700",
+              "700i"
+            ],
+            "subsets": [
+              "latin-ext"
+            ]
+          }
+        ],
+        "formats": [
+          "woff",
+          "woff2"
+        ]
+      },
+    },
     
     
     {
